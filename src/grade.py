6a79e03b4c61660b2906ede90504e686fb845b10
yixuan <sherryni95@gmail.com>
2018-10-08 17:51:41 -0500

    change grade

--- src/grade.py
+++ src/grade.py
@@ -1,14 +1,17 @@
 from __future__ import print_function
-from os import path
+import os
 from math import sin, cos, atan2, sqrt, pi
 from run import *
 from util import *
+import random
+random.seed(42)
 
 SRC_PATH = path.dirname(path.abspath(__file__))
 DATA_PATH = path.join(SRC_PATH, '..', 'data')
 YEARBOOK_PATH = path.join(DATA_PATH, 'yearbook')
 YEARBOOK_VALID_PATH = path.join(YEARBOOK_PATH, 'valid')
 YEARBOOK_TEST_PATH = path.join(YEARBOOK_PATH, 'test')
+YEARBOOK_TRAIN_PATH = path.join(YEARBOOK_PATH, 'train')
 YEARBOOK_TEST_LABEL_PATH = path.join(SRC_PATH, '..', 'output', 'yearbook_test_label.txt')
 STREETVIEW_PATH = path.join(DATA_PATH, 'geo')
 STREETVIEW_VALID_PATH = path.join(STREETVIEW_PATH, 'valid')
@@ -49,25 +52,55 @@ def dist(lat1, lon1, lat2, lon2):
 
 
 # Evaluate L1 distance on valid data for yearbook dataset
-def evaluateYearbook(Predictor):
+"""def evaluateYearbook(predictor):
     test_list = util.listYearbook(False, True)
-    predictor = Predictor()
+    #predictor = Predictor()
     predictor.DATASET_TYPE = 'yearbook'
 
     total_count = len(test_list)
     l1_dist = 0.0
     print("Total validation data", total_count)
     count = 0
-    for image_gr_truth in test_list:
-        image_path = path.join(YEARBOOK_VALID_PATH, image_gr_truth[0])
-        pred_year = predictor.predict(image_path)
-        truth_year = int(image_gr_truth[1])
-        l1_dist += abs(pred_year[0] - truth_year)
-        count = count + 1
 
-    l1_dist /= total_count
-    print("L1 distance", l1_dist)
-    return l1_dist
+    folder_path = path.join(SRC_PATH, '..', 'output', predictor.model_name)
+    if not os.path.isdir(folder_path):
+        os.mkdir(folder_path)
+    output_path = path.join(folder_path, 'yearbook_validation_label.txt')
+
+    with open(output_path, 'a') as f:
+        for image_gr_truth in test_list:
+            image_path = path.join(YEARBOOK_VALID_PATH, image_gr_truth[0])
+            pred_year = predictor.predict(image_path)
+            truth_year = int(image_gr_truth[1])
+            l1_dist += abs(pred_year[0] - truth_year)
+            count = count + 1
+            print('%s,%i,%i,%i\n'%(image_gr_truth[0], pred_year[0], truth_year, abs(pred_year[0] - truth_year)))
+            f.write('%s,%i,%i,%i\n'%(image_gr_truth[0], pred_year[0], truth_year, abs(pred_year[0] - truth_year)))
+
+        l1_dist /= total_count
+        print("L1 distance", l1_dist)
+        f.write(str(l1_dist))
+    return l1_dist"""
+
+def evaluateYearbook(Predictor):
+  test_list = util.listYearbook(False, True)
+  predictor = Predictor()
+  predictor.DATASET_TYPE = 'yearbook'
+
+  total_count = len(test_list)
+  l1_dist = 0.0
+  print( "Total validation data", total_count )
+  count=0
+  for image_gr_truth in test_list:
+    image_path = path.join(YEARBOOK_VALID_PATH, image_gr_truth[0])
+    pred_year = predictor.predict(image_path)
+    truth_year = int(image_gr_truth[1])
+    l1_dist += abs(pred_year[0] - truth_year)
+    count=count+1
+
+  l1_dist /= total_count
+  print( "L1 distance", l1_dist)
+  return l1_dist
 
 
 # Evaluate L1 distance on valid data for geolocation dataset
@@ -88,24 +121,21 @@ def evaluateStreetview(Predictor):
     print("L1 distance", l1_dist)
     return l1_dist
 
-
-# Predict label for test data on yearbook dataset
 def predictTestYearbook(Predictor):
-    test_list = util.testListYearbook()
-    predictor = Predictor()
-    predictor.DATASET_TYPE = 'yearbook'
+  test_list = util.testListYearbook()
+  predictor = Predictor()
+  predictor.DATASET_TYPE = 'yearbook'
 
-    total_count = len(test_list)
-    print("Total test data: ", total_count)
+  total_count = len(test_list)
+  print( "Total test data: ", total_count )
 
-    output = open(YEARBOOK_TEST_LABEL_PATH, 'w')
-    for image in test_list:
-        image_path = path.join(YEARBOOK_TEST_PATH, image[0])
-        pred_year = predictor.predict(image_path)
-        out_string = image[0] + '\t' + str(pred_year[0]) + '\n'
-        output.write(out_string)
-    output.close()
-
+  output = open(YEARBOOK_TEST_LABEL_PATH, 'w')
+  for image in test_list:
+    image_path = path.join(YEARBOOK_TEST_PATH, image[0])
+    pred_year = predictor.predict(image_path)
+    out_string = image[0] + '\t' + str(pred_year[0]) + '\n'
+    output.write(out_string)
+  output.close()
 
 # Predict label for test data for geolocation dataset
 def predictTestStreetview(Predictor):
@@ -134,8 +164,15 @@ if __name__ == "__main__":
                         help="Dataset: yearbook/geolocation", required=True)
     parser.add_argument("--type", dest="type",
                         help="Dataset: valid/test", required=True)
+    parser.add_argument("--model", dest="model",
+                        help="Model name", default='trained_graph.pb')
+    parser.add_argument("--label_path", dest="label_path",
+                        default="trained_labels.txt")
+    parser.add_argument("--data_type", dest="data_type",
+                        default="year")
 
     args = parser.parse_args()
+
     if args.dataset_type == 'yearbook':
         print("Yearbook")
         if (args.type == 'valid'):
